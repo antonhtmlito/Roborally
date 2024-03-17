39f6ccfdd4e524e684f6fc5275031702dc9e76c8
wilfip <william.kofoed@gmail.com>
2024-03-17 22:09:51 +0100

    implementeret vægge

--- src/main/java/dk/dtu/compute/se/pisd/roborally/view/SpaceView.java
+++ src/main/java/dk/dtu/compute/se/pisd/roborally/view/SpaceView.java
@@ -27,9 +27,12 @@ import dk.dtu.compute.se.pisd.roborally.model.Player;
 import dk.dtu.compute.se.pisd.roborally.model.Space;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.Line;
 import javafx.scene.shape.Polygon;
+import javafx.scene.shape.Rectangle;
 import javafx.scene.shape.StrokeLineCap;
 import org.jetbrains.annotations.NotNull;
 
@@ -37,7 +40,6 @@ import org.jetbrains.annotations.NotNull;
  * ...
  *
  * @author Ekkart Kindler, ekki@dtu.dk
- *
  */
 public class SpaceView extends StackPane implements ViewObserver {
 
@@ -65,6 +67,7 @@ public class SpaceView extends StackPane implements ViewObserver {
             this.setStyle("-fx-background-color: black;");
         }
 
+
         // updatePlayer();
 
         // This space view should listen to changes of the space
@@ -79,14 +82,14 @@ public class SpaceView extends StackPane implements ViewObserver {
         if (player != null) {
             Polygon arrow = new Polygon(0.0, 0.0,
                     10.0, 20.0,
-                    20.0, 0.0 );
+                    20.0, 0.0);
             try {
                 arrow.setFill(Color.valueOf(player.getColor()));
             } catch (Exception e) {
                 arrow.setFill(Color.MEDIUMPURPLE);
             }
 
-            arrow.setRotate((90*player.getHeading().ordinal())%360);
+            arrow.setRotate((90 * player.getHeading().ordinal()) % 360);
             this.getChildren().add(arrow);
         }
     }
@@ -95,6 +98,48 @@ public class SpaceView extends StackPane implements ViewObserver {
     public void updateView(Subject subject) {
         if (subject == this.space) {
             updatePlayer();
+        }
+        //@S235451
+        if (space.x == 2 && space.y == 2) {
+
+            Pane pane = new Pane();
+            Rectangle rectangle =
+                    new Rectangle(0.0, 0.0, SPACE_WIDTH, SPACE_HEIGHT);
+            rectangle.setFill(Color.TRANSPARENT);
+            pane.getChildren().add(rectangle);
+            // SOUTH
+            Line SOUTHline =
+                    new Line(2, SPACE_HEIGHT -2,
+                            SPACE_WIDTH-2, SPACE_HEIGHT-2);
+            SOUTHline.setStroke(Color.RED);
+            SOUTHline.setStrokeWidth(5);
+            pane.getChildren().add(SOUTHline);
+            // EAST
+            Line eastLine = new Line(SPACE_WIDTH - 2, 2,
+                    SPACE_WIDTH - 2, SPACE_HEIGHT - 2);
+            eastLine.setStroke(Color.RED);
+            eastLine.setStrokeWidth(5);
+            pane.getChildren().add(eastLine);
+            // NORTH
+            Line NorthLine = new Line(2, 2,
+                    SPACE_WIDTH - 2, 2);
+            NorthLine.setStroke(Color.RED);
+            NorthLine.setStrokeWidth(5);
+            pane.getChildren().add(NorthLine);
+            // WEST
+            Line WESTLine = new Line(2, 2,
+                   2 , SPACE_HEIGHT - 2);
+            WESTLine.setStroke(Color.RED);
+            WESTLine.setStrokeWidth(5);
+            pane.getChildren().add(WESTLine);
+
+
+            this.getChildren().add(pane);
+
+
+
+
+
         }
     }
 
