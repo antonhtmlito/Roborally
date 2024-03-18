dd51ad7444f8a8dda884165ddfe803d70febd4af
wilfip <william.kofoed@gmail.com>
2024-03-18 19:18:11 +0100

    merge konflikt løst

--- src/main/java/dk/dtu/compute/se/pisd/roborally/view/SpaceView.java
+++ src/main/java/dk/dtu/compute/se/pisd/roborally/view/SpaceView.java
@@ -28,9 +28,12 @@ import dk.dtu.compute.se.pisd.roborally.model.Space;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.layout.Background;
+import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.Line;
 import javafx.scene.shape.Polygon;
+import javafx.scene.shape.Rectangle;
 import javafx.scene.shape.StrokeLineCap;
 import org.jetbrains.annotations.NotNull;
 
@@ -94,13 +97,52 @@ public class SpaceView extends StackPane implements ViewObserver {
             this.setStyle("-fx-background-color: brown;");
         }
     }
-
+    //@S235451
     @Override
     public void updateView(Subject subject) {
-
         if (subject == this.space) {
             updatePlayer();
-            updateCheckpoint();
+        }
+        if (space.x == 2 && space.y == 2) {
+
+            Pane pane = new Pane();
+            Rectangle rectangle =
+                    new Rectangle(0.0, 0.0, SPACE_WIDTH, SPACE_HEIGHT);
+            rectangle.setFill(Color.TRANSPARENT);
+            pane.getChildren().add(rectangle);
+            // SOUTH
+            Line SOUTHline =
+                    new Line(2, SPACE_HEIGHT -2,
+                            SPACE_WIDTH-2, SPACE_HEIGHT-2);
+            SOUTHline.setStroke(Color.RED);
+            SOUTHline.setStrokeWidth(5);
+            pane.getChildren().add(SOUTHline);
+            // EAST
+            Line eastLine = new Line(SPACE_WIDTH - 2, 2,
+                    SPACE_WIDTH - 2, SPACE_HEIGHT - 2);
+            eastLine.setStroke(Color.RED);
+            eastLine.setStrokeWidth(5);
+            pane.getChildren().add(eastLine);
+            // NORTH
+            Line NorthLine = new Line(2, 2,
+                    SPACE_WIDTH - 2, 2);
+            NorthLine.setStroke(Color.RED);
+            NorthLine.setStrokeWidth(5);
+            pane.getChildren().add(NorthLine);
+            // WEST
+            Line WESTLine = new Line(2, 2,
+                    2 , SPACE_HEIGHT - 2);
+            WESTLine.setStroke(Color.RED);
+            WESTLine.setStrokeWidth(5);
+            pane.getChildren().add(WESTLine);
+
+
+            this.getChildren().add(pane);
+
+
+
+
+
         }
     }
 
