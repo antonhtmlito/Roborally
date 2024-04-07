20c34bfec6385577cedb1b74ed35b3dc1d301dc3
antonhtmlito <antondong04@gmail.com>
2024-04-07 23:04:01 +0200

    update module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -6,12 +6,29 @@ module roborally {
 
     requires javafx.controls;
     requires org.jetbrains.annotations;
+    requires java.sql;
+    requires mysql.connector.j;
+    requires com.google.common;
+    requires com.google.gson;
 
     exports dk.dtu.compute.se.pisd.roborally;
 
+    opens schemas;
+    opens boards;
+
+    // The following are needed for Gson being able to create the
+    // respective Java objects when reading JSON files
     exports dk.dtu.compute.se.pisd.roborally.model;
+    exports dk.dtu.compute.se.pisd.roborally.fileaccess.model;
+
+    // the following are needed only for generating the JavaDocs via maven
+    // for the respective packages
     exports dk.dtu.compute.se.pisd.roborally.view;
     exports dk.dtu.compute.se.pisd.roborally.controller;
 
+    exports dk.dtu.compute.se.pisd.roborally.dal;
+    exports dk.dtu.compute.se.pisd.roborally.fileaccess;
+
     exports dk.dtu.compute.se.pisd.designpatterns.observer;
+
 }
